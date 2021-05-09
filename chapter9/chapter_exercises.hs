0d9795ec63dc2a54426f81034a9efcdd05b3c6f0
tryingtolearn11 <damiensingh17@gmail.com>
2021-05-09 16:09:03 -0400

    1st p

--- chapter9/chapter_exercises.hs
+++ chapter9/chapter_exercises.hs
@@ -18,10 +18,16 @@ capitalize_all :: String -> String
 capitalize_all [] = []
 capitalize_all (x : xs) = (toUpper x) : capitalize_all xs
 
+-- 5 --
+capitalize_head :: [Char] -> Char
+capitalize_head xs = toUpper $ (head xs)
 
+-- 6 --
+capitalize_head' :: [Char] -> Char
+capitalize_head' xs = (toUpper . head) xs
 
-
-
+capitalize_head_pointfree :: [Char] -> Char
+capitalize_head_pointfree = (toUpper . head)
 
 
 
