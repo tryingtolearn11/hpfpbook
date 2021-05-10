48d7b90da33a3973d40e734225c926c7002730b7
tryingtolearn11 <damiensingh17@gmail.com>
2021-05-09 20:47:14 -0400

    cipher decipher

--- chapter9/cipher.hs
+++ chapter9/cipher.hs
@@ -12,3 +12,11 @@ right_shift_cipher (x : xs) (k)
   | (ord x + k) == (ord 'z') = chr (ord x + k) : right_shift_cipher xs k
   | otherwise                = chr ((ord x + k) `mod` 122) : right_shift_cipher xs k
 
+
+
+right_shift_decipher :: String -> Int -> String
+right_shift_decipher [] _ = []
+right_shift_decipher (' ': xs) k = right_shift_decipher xs k
+right_shift_decipher (x : xs) (k)
+  | (ord x - k) < (ord 'a') = chr ((122) - (96 - (((ord x - k) `mod` 122)))) : right_shift_decipher xs k
+  | otherwise               = chr (( ord x - k) `mod` 122) : right_shift_decipher xs k
