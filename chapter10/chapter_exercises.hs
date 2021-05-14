09e801b8cd0918e3801c60367a9ab2eaf79f7a13
tryingtolearn11 <damiensingh17@gmail.com>
2021-05-14 17:27:36 -0400

    ex

--- chapter10/chapter_exercises.hs
+++ chapter10/chapter_exercises.hs
@@ -3,7 +3,42 @@
 module ChapterExercises where
 
 -- 1 --
+-- function broken idk
+-- TODO : come back to solve this lol
 f [] _ = []
 f (_:[]) _ = []
-f (x : xy) z = (x, z, (head xy)) : (head xy, z, x) : f (xy) z
+f (x : xy) z = (x, z, x) : (x, z, (head xy)) : (head xy, z, x) : (head xy, z, last xy) :(x, z, last xy) : f (xy) z
+
+g = map . f
+
+-- 2 --
+xs :: String
+xs = "I love cats"
+
+
+seekrItFunc :: String -> Int
+seekrItFunc x =
+    div (sum (map length (words x))) (length (words x))
+
+
+-- 3 --
+seekrItFunc' :: Fractional a => String -> a
+seekrItFunc' x = (fromIntegral(sum (map length (words x)))) / (fromIntegral (length (words x)))
+
+-- Rewriting --
+-- 1 --
+myOr :: [Bool] -> Bool
+myOr = foldr (||) False
+
+
+-- 2 --
+myAny :: (a -> Bool) -> [a] -> Bool 
+myAny f = foldr ((||) . f) False
+
+-- 3 --
+myElem :: Eq a => a -> [a] -> Bool
+myElem x ys = foldr ((||) . (== x)) False ys
+
+myElem' :: Eq a => a -> [a] -> Bool
+myElem' x ys = any (==x) ys
 
