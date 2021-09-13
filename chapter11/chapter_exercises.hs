50629be68dc8626608ddf81eb6652e0ed65d8fb6
tryingtolearn11 <damiensingh17@gmail.com>
2021-09-13 19:07:06 -0400

    resuming

--- chapter11/chapter_exercises.hs
+++ chapter11/chapter_exercises.hs
@@ -13,10 +13,13 @@ data Weekday = Monday
              | Friday
 
 
-f :: Weekday -> String
-f Friday = "Miller Time"
+-- f :: Weekday -> String
+-- f Friday = "Miller Time"
 
-
+-- 4 --
+-- the function g delivers the final element from a list
+g :: [a] -> a
+g xs = xs !! (length xs - 1)
 
 -- Vigenere Ciper --
 
@@ -53,6 +56,54 @@ ciper xs keyword = go xs (keystream xs keyword) " " 0 (length xs)
           
 
 -- As-patterns --
+{-
+
+   As-patterns are a nifty way to be able to pattern match
+   on part of something and still refer to the entire original value.
+
+-}
+
+-- Practice --
+-- Here we pattern matched on a tuple so we could get at the first value
+-- for printing, but used the @ symbol to introduce a binding named 't' in
+-- order to refer to the whole tuple rather than just a part.
+
+f :: Show a => (a, b) -> IO (a, b)
+f t@(a, _) = do
+    print a
+    return t
+
+-- Without As-Pattern
+f' :: Show a => (a, b) -> IO (a, b)
+f' (a, b) = do
+    print a
+    return (a, b)
+
+
+-- We use As-Patterns w/ pattern matching here on arbitrary data constructors
+-- (lists in this case)
+doubleUp :: [a] -> [a]
+doubleUp [] = []
+doubleUp xs@(x:_) = x : xs
+
+
+-- Without As-Pattern
+doubleUp' :: [a] -> [a]
+doubleUp' [] = []
+doubleUp' (x: zy) = x : (x : zy)
+
+
+
+
+
+
+
+
+
+
+
+
+
 -- TODO: understand as-patterns more
 isSubSeqOf :: (Eq a)
            => [a]
@@ -63,29 +114,6 @@ isSubSeqOf _ [] = False
 isSubSeqOf xs@(x : xz) (y : yz)
   | x == y    = isSubSeqOf xz yz
   | otherwise = isSubSeqOf xs yz
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- chapter11/types.hs
+++ chapter11/types.hs
@@ -1,7 +1,33 @@
 -- types.hs --
 
 module Types where
+{-
 
+Data Declaration Breakdown
+
+    data Bool = False | True
+     [1] [2] [3] [4] [5] [6]  
+    
+    
+    data [] a = [] | a : [a]
+         [7]    [8]   [9]
+
+
+[1] - keyword 'data' signals a Data Declaration / declaration of datatype
+[2] - Type Construtor (w/ no arguments)
+[3] - = Equal Sign divides the type constructor from its data constructors
+[4] - Data Constructor (one that takes no arguments is a 'nullary constructor'
+[5] - The Pipe denotes a sum type (logical disjunction OR) 
+[6] - nullary constructor for the value True
+
+[7] - Type Constructor with an argument. An empty list has to be applied 
+      to an argument in order to become a list of something. Here the argument
+      is a polymorphic type variable, so the list's argument can be of different
+      types.
+[8] - Data Constructor for empty list
+[9] - Data Constructor that takes 2 arguments: a and [a]
+
+-}
 data Price = Price Integer deriving (Eq, Show)
 
 data Manufacturer = Mini | Mazda | Tata deriving (Eq, Show)
