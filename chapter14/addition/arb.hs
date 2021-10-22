55a3a28ae5a64e7393de9642ffdd8e07b6e15ca3
tryingtolearn11 <damiensingh17@gmail.com>
2021-10-22 16:08:29 -0400

    arb instances

---
+++ chapter14/addition/arb.hs
@@ -0,0 +1,59 @@
+module Main where
+
+import Test.QuickCheck
+
+data Trivial =
+    Trivial deriving (Eq, Show)
+
+trivialGen :: Gen Trivial
+trivialGen = return Trivial
+
+
+instance Arbitrary Trivial where
+    arbitrary = trivialGen
+
+
+data Identity a =
+    Identity a deriving (Eq, Show)
+
+identityGen :: Arbitrary a =>
+               Gen (Identity a)
+
+identityGen = do
+    a <- arbitrary
+    return (Identity a)
+
+instance Arbitrary a =>
+    Arbitrary (Identity a) where
+        arbitrary = identityGen
+
+
+identityGenInt :: Gen (Identity Int)
+identityGenInt = identityGen
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main :: IO ()
+main = do
+    sample trivialGen
+
+
+
